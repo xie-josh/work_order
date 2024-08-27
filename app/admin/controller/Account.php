a6ad133dd57e0b8bd43b646497bad50f31cfa204
Your Name <you@example.com>
2024-08-27 14:08:03 +0800

    fix:优化

--- app/admin/controller/Account.php
+++ app/admin/controller/Account.php
@@ -21,7 +21,7 @@ class Account extends Backend
     protected array|string $preExcludeFields = ['id', 'account_id', 'admin_id', 'create_time', 'update_time'];
 
     protected array $withJoinTable = ['admin'];
-    protected array $noNeedPermission = ['accountCountMoney','editIs_','audit','index'];
+    protected array $noNeedPermission = ['accountCountMoney','editIs_','audit','index','getAccountNumber'];
     protected string|array $quickSearchField = ['id'];
 
     protected bool|string|int $dataLimit = 'parent';
