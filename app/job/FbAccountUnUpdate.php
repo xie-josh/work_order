f97572a213858f46bdb0d896848e0112ea40996b
Your Name <you@example.com>
2025-02-12 17:54:43 +0800

    fix:优化

--- app/job/FbAccountUnUpdate.php
+++ app/job/FbAccountUnUpdate.php
@@ -34,15 +34,15 @@ class FbAccountUnUpdate
             $id = $params['id'];
 
             $_is = true;
+            if($params['type'] == 1){
+                $token = (new \app\admin\services\fb\FbService())->getPersonalbmToken(1,$id);
+                $params['token'] = $token;
+            }else if($params['type'] == 2){
+                $token = (new \app\admin\services\fb\FbService())->getPersonalbmToken(2,$id);
+                $params['token'] = $token;
+            }
             while ($_is) {
                 $params['account_status'] = 1;
-                if($params['type'] == 1){
-                    $token = (new \app\admin\services\fb\FbService())->getPersonalbmToken(1,$id);
-                    $params['token'] = $token;
-                }else if($params['type'] == 2){
-                    $token = (new \app\admin\services\fb\FbService())->getPersonalbmToken(2,$id);
-                    $params['token'] = $token;
-                }
                 $result = (new \app\services\FacebookService())->list($params);
                 if(empty($result) || $result['code'] == 0) return true;
 
