49a7f7c606b89eb33f873f75a1e05bc3030f0484
Your Name <you@example.com>
2025-03-31 14:09:33 +0800

    fix:优化

---
+++ app/admin/model/addaccountrequest/AccountCard.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace app\admin\model\addaccountrequest;
+
+use think\Model;
+
+/**
+ * Accountrequest
+ */
+class AccountCard extends Model
+{
+    // 表名
+    protected $name = 'account_card';
+
+    // 自动写入时间戳字段
+    protected $autoWriteTimestamp = true;
+
+
+}
