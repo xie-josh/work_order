27d2dcfb508995f2720d12010f4a3793ff2df78c
Your Name <you@example.com>
2025-04-16 11:25:15 +0800

    fax:优化

---
+++ app/admin/controller/addaccountrequest/AccountReturn.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace app\admin\controller\addaccountrequest;
+
+use Throwable;
+use app\common\controller\Backend;
+use think\facade\Db;
+
+
+class AccountReturn extends Backend
+{
+    /**
+     * AccountrequestProposal模型对象
+     * @var object
+     * @phpstan-var \app\admin\model\demand\BcTkModel
+     */
+    protected object $model;
+
+    protected array|string $preExcludeFields = ['id', 'create_time', 'update_time'];
+
+    protected array $withJoinTable = [];
+
+    protected string|array $quickSearchField = ['id'];
+
+    protected array $noNeedPermission = [];
+
+    protected bool|string|int $dataLimit = 'parent';
+
+    public function initialize(): void
+    {
+        parent::initialize();
+        $this->model = new \app\admin\model\addaccountrequest\AccountReturnModel();
+    }
+
+
+    public function audit(): void
+    {
+        
+        if($this->request->isPost()){
+            $data = $this->request->post();
+            if(empty($data['ids']) || empty($data['status'])) $this->error('参数错误！');
+            
+            $resutn = $this->model->whereIn('id',$data['ids'])->update(['status'=>$data['status']]);
+
+            if($resutn){
+                $this->success('操作成功！');
+            }else{
+                $this->error('操作失败！');
+            }
+        }
+        $this->error('操作失败！');
+    }
+
+
+    public function edit(): void
+    {
+        $this->error('功能未开放！');
+    }
+
+    public function del(array $ids = []): void
+    
+    {
+        $this->error('功能未开放！');
+    }
+
+
+
+}
---
+++ app/admin/model/addaccountrequest/AccountReturnModel.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace app\admin\model\addaccountrequest;
+
+use think\Model;
+
+/**
+ * Transfer
+ */
+class AccountReturnModel extends Model
+{
+    // 表名
+    protected $name = 'account_return';
+
+    // 自动写入时间戳字段
+    protected $autoWriteTimestamp = true;
+    
+}
