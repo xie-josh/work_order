ea07a9ff8c3828477a3b70ec8c44401d8ff0482e
Your Name <you@example.com>
2025-04-18 18:30:35 +0800

    fix:优化

--- app/admin/controller/fb/FbLogs.php
+++ app/admin/controller/fb/FbLogs.php
@@ -5,6 +5,7 @@ namespace app\admin\controller\fb;
 
 use app\common\controller\Backend;
 use think\facade\Db;
+use think\facade\Cache;
 
 class FbLogs extends Backend
 {
@@ -35,25 +36,16 @@ class FbLogs extends Backend
 
         list($where, $alias, $limit, $order) = $this->queryBuilder();
 
-        //array_push($where,['accountrequest_proposal.processing_status','=',0]);
         array_push($where,['fb_logs_model.type','IN',['FB_insights']]);
-        //array_push($where,['fb_logs_model.create_time','>',date('Y-m-d',strtotime('-30 days'))]);
-        //dd($where);
 
         $res = DB::table('ba_fb_logs')
-            ->field('fb_logs_model.type,fb_logs_model.log_id,accountrequest_proposal.account_id,accountrequest_proposal.bm,accountrequest_proposal.status,accountrequest_proposal.serial_name,fb_logs_model.logs,admin.nickname,account.open_money,fb_logs_model.create_time,accountrequest_proposal.processing_status,accountrequest_proposal.processing_amount,accountrequest_proposal.processing_time,accountrequest_proposal.comment')
+            ->field('fb_logs_model.status,fb_logs_process.amout logs_process_amout,fb_logs_process.comment logs_process_comment,fb_logs_process.create_time logs_process_create_time,fb_logs_model.type,fb_logs_model.log_id,accountrequest_proposal.account_id,accountrequest_proposal.bm,accountrequest_proposal.serial_name,accountrequest_proposal.currency,fb_logs_model.logs,admin.nickname,account.open_money,account.money,fb_logs_model.create_time')
             ->leftJoin('ba_accountrequest_proposal accountrequest_proposal','accountrequest_proposal.account_id=fb_logs_model.log_id')
             ->leftJoin('ba_account account','account.account_id=accountrequest_proposal.account_id')
             ->leftJoin('ba_admin admin','admin.id=account.admin_id')
+            ->leftJoin('ba_fb_logs_process fb_logs_process','fb_logs_process.fb_logs_id=fb_logs_model.id')
             ->alias($alias)
             ->where($where)
-            // ->where('fb_logs_model.id','in',function($query){
-            //     $query->table('ba_fb_logs')->field('MAX(id) as max_id')->where([
-            //         ['type','IN',['FB_insights','FB_accountStatus']],
-            //         ['create_time','>',date('Y-m-d',strtotime('-30 days'))],
-            //         //['logs','NOT LIKE','%Application request limit reached%'],
-            //     ])->group('log_id');
-            // })
             ->order('fb_logs_model.id','desc')
             ->paginate($limit);
             
@@ -61,40 +53,39 @@ class FbLogs extends Backend
 
         if($dataList)
         {
-            $accountListIds = array_column($dataList,'account_id');
-             
-            //     1.fb余额(（总充值 + 首充） - 总扣款 - 总消费账单 - 总清零)
-            $recharge = DB::table('ba_recharge')->field('sum(number) number,account_id')->whereIn('account_id',$accountListIds)->where('type',1)->where('status',1)->group('account_id')->select()->toArray();
-            //$recharge1 = DB::table('ba_account')->field('open_money,account_id')->whereIn('account_id',$accountListIds)->select()->toArray();
-            $recharge2 = DB::table('ba_recharge')->field('sum(number) number,account_id')->whereIn('account_id',$accountListIds)->where('type',2)->where('status',1)->group('account_id')->select()->toArray();
-            //$recharge3 = DB::table('ba_account_consumption')->field('sum(spend) spend,account_id')->whereIn('account_id',$accountListIds)->group('account_id')->select()->toArray();
-            $recharge4 = DB::table('ba_recharge')->field('sum(number) number,account_id')->whereIn('account_id',$accountListIds)->whereIn('type',[3,4])->where('status',1)->group('account_id')->select()->toArray();
-
-            $recharge = array_column($recharge,'number','account_id');
-            //$recharge1 = array_column($recharge1,'open_money','account_id');
-            $recharge2 = array_column($recharge2,'number','account_id');
-            //$recharge3 = array_column($recharge3,'spend','account_id');
-            $recharge4 = array_column($recharge4,'number','account_id');
+            $currencyRate = config('basics.currency');
 
             foreach($dataList as &$v){
-                $totalRecharge = $recharge[$v['account_id']]??0;
-                //$firshflush = $recharge1[$v['account_id']]??0;
-                $totalDeductions = $recharge2[$v['account_id']]??0;
-                //$totalConsumption = $recharge3[$v['account_id']]??0;
-                $totalReset = $recharge4[$v['account_id']]??0;
-                $openAmount = empty($v['open_money'])?0:$v['open_money'];
 
-                $v['total_recharge_amount'] = ($totalRecharge + $openAmount) - $totalDeductions;
-                $v['total_reset'] = $totalReset;
+                $currencyNumber =  '';
+                if(!empty($currencyRate[$v['currency']])){
+                    $currencyNumber = bcmul((string)$v['money'], $currencyRate[$v['currency']],2);
+                }else{
+                    $currencyNumber = (string)$v['money'];
+                }
+                $v['currency_account'] = $currencyNumber;
+
+                 /**
+         * 
+            管理BM  accountrequest_proposal->bm (可搜索)
+            账户名称  accountrequest_proposal->serial_name (可搜索)
+            账户ID   accountrequest_proposal->account_id (可搜索)
+            开户时间 open_money
+            归属用户 admin->nickname
+            总充值  currency_account
+            总消耗    logs_process_amout  
+            货币      accountrequest_proposal->currency (可搜索)
+            创建时间  create_time
+            处理状态  fb_logs_status (可搜索)
+            处理时间  logs_process_create_time
+            备注    logs_process_comment
+         */
 
                 $v['accountrequest_proposal']['account_id'] = $v['account_id'];
                 $v['accountrequest_proposal']['bm'] = $v['bm'];
                 $v['accountrequest_proposal']['status'] = $v['status'];
                 $v['accountrequest_proposal']['serial_name'] = $v['serial_name'];
-                $v['accountrequest_proposal']['processing_status'] = $v['processing_status'];
-                $v['accountrequest_proposal']['processing_amount'] = $v['processing_amount'];
-                $v['accountrequest_proposal']['processing_time'] = $v['processing_time'];
-                $v['accountrequest_proposal']['comment'] = $v['comment'];
+                $v['accountrequest_proposal']['currency'] = $v['currency'];
                 $v['admin']['nickname'] = $v['nickname'];
 
             }
@@ -113,24 +104,36 @@ class FbLogs extends Backend
             $result = false;
             $this->model->startTrans();
             try {
-                $accountId = $data['account_id']??[];
+                $accountId = $data['account_id'];
                 $status = $data['status']??'';
-                $processingStatus = $data['processing_status']??1;
                 $processingAmount = $data['processing_amount']??0;
                 $comment = $data['comment']??'';
                 
-                if(!$accountId || !$processingStatus) throw new \Exception("Error Processing Request");
+                // $fblogsModel = new \app\admin\model\fb\FbLogsModel();
+                if(!$accountId) throw new \Exception("Error Processing Request");
 
-                $dataList = [
-                    'processing_status'=>$processingStatus,
-                    'processing_amount'=>$processingAmount,
-                    'processing_time'=>date('Y-m-d H:i',time()),
-                ];
+                $accountIds = array_unique($accountId);
+
+                $fbLogsList = $this->model->field('id')->whereIn('log_id',$accountIds)->where('type','FB_insights')->where('status',0)->select()->toArray();
+
+                $fbLogsIds = array_column($fbLogsList,'id');
+
+                $data = [];
+                foreach($fbLogsList as $v)
+                {
+                    $data[] = [
+                        'fb_logs_id'=>$v['id'],
+                        'amout'=>$processingAmount,
+                        'comment'=>$comment,
+                        'create_time'=>date('Y-m-d H:i:d',time()),
+                    ];
+                }
+
+                if(!empty($fbLogsIds)) $this->model->whereIn('id',$fbLogsIds)->update(['status'=>1]);
+                if(!empty($data)) DB::table('ba_fb_logs_process')->insertAll($data);               
                 
-                if(isset($status)) $dataList['status'] = $status;
-                if(isset($comment)) $dataList['comment'] = $comment;
-                
-                $result = DB::table('ba_accountrequest_proposal')->where('processing_status',0)->whereIn('account_id',$accountId)->update($dataList);
+                if(!empty($status)) DB::table('ba_accountrequest_proposal')->whereIn('account_id',$accountIds)->update(['status'=>$status]);
+                $result = true;
                 $this->model->commit();
             } catch (\Throwable $e) {
                 $this->model->rollback();
@@ -145,5 +148,114 @@ class FbLogs extends Backend
     }
 
 
+    public function lostNumber()
+    {
+        $where = [
+            ['type','IN',['FB_insights']],
+        ];
+        $numbertotal = $this->model->where($where)->where([['create_time','like',"%".date('Y-m-d',time())."%"]])->group('log_id')->count('id');
+        $numberNotProcess = $this->model->where($where)->where('status',0)->group('log_id')->count('id');
+
+        $this->success('', [
+            'number_total' => $numbertotal,
+            'number_not_process' => $numberNotProcess,
+        ]);
+    } 
+
+    public function export()
+    {
+        $where = [];
+        set_time_limit(300);
+        list($where, $alias, $limit, $order) = $this->queryBuilder();
+
+        $batchSize = 2000;
+        $processedCount = 0;
+        $redisKey = 'export_progress'.'_'.$this->auth->id;
+        
+        array_push($where,['fb_logs_model.type','IN',['FB_insights']]);
+
+        $query = DB::table('ba_fb_logs')
+            ->field('fb_logs_model.status fb_logs_status,fb_logs_process.amout logs_process_amout,fb_logs_process.comment logs_process_comment,fb_logs_process.create_time logs_process_create_time,fb_logs_model.type,fb_logs_model.log_id,accountrequest_proposal.account_id,accountrequest_proposal.bm,accountrequest_proposal.status,accountrequest_proposal.serial_name,accountrequest_proposal.currency,fb_logs_model.logs,admin.nickname,account.open_money,account.money,fb_logs_model.create_time')
+            ->leftJoin('ba_accountrequest_proposal accountrequest_proposal','accountrequest_proposal.account_id=fb_logs_model.log_id')
+            ->leftJoin('ba_account account','account.account_id=accountrequest_proposal.account_id')
+            ->leftJoin('ba_admin admin','admin.id=account.admin_id')
+            ->leftJoin('ba_fb_logs_process fb_logs_process','fb_logs_process.fb_logs_id=fb_logs_model.id')
+            ->alias($alias)
+            ->where($where)
+            ->order('fb_logs_model.id','desc');
+
+        $total = $query->count(); 
+
+        $statusValue = [0=>'未处理',1=>'处理完成'];
+        $currencyRate = config('basics.currency');
+
+        $folders = (new \app\common\service\Utils)->getExcelFolders();
+        $header = [
+            '管理BM',
+            '账户名称',
+            '账户ID',
+            '开户时间',
+            '归属用户',
+            '总充值',
+            '总消耗',
+            '货币',
+            '创建时间',
+            '处理状态',
+            '处理时间',
+            '备注'
+        ];
+
+        $config = [
+            'path' => $folders['path']
+        ];
+        $excel  = new \Vtiful\Kernel\Excel($config);
+
+        $name = $folders['name'].'.xlsx';
+
+        for ($offset = 0; $offset < $total; $offset += $batchSize) {
+            $data = $query->limit($offset, $batchSize)->select()->toArray();
+            $dataList=[];
+            foreach($data as $v){
+                $currencyNumber =  '';
+                if(!empty($currencyRate[$v['currency']])){
+                    $currencyNumber = bcmul((string)$v['money'], $currencyRate[$v['currency']],2);
+                }else{
+                    $currencyNumber = (string)$v['money'];
+                }
+
+                $dataList[] = [
+                    $v['bm'],
+                    $v['serial_name'],
+                    $v['account_id'],
+                    empty($v['open_money'])?'':date('Y-m-d H:i:s',$v['open_money']),
+                    $v['nickname'],
+                    $currencyNumber,
+                    $v['logs_process_amout'],
+                    $v['currency'],
+                    $v['create_time'],
+                    $statusValue[$v['fb_logs_status']]??'未知状态',
+                    $v['logs_process_create_time'],
+                    $v['logs_process_comment'],
+                ];  
+                $processedCount++;
+            }
+            $excel->fileName($folders['name'].'.xlsx', 'sheet1')
+            ->header($header)
+            ->data($dataList);
+            $progress = min(100, ceil($processedCount / $total * 100));
+            Cache::store('redis')->set($redisKey, $progress, 300);
+        }
+
+        $excel->output();
+        Cache::store('redis')->delete($redisKey);
+
+        $this->success('',['path'=>$folders['filePath'].'/'.$name]);
+    }
+
+    public function getExportProgress()
+    {
+        $progress = Cache::store('redis')->get('export_progress'.'_'.$this->auth->id, 0);
+        return $this->success('',['progress' => $progress]);
+    }
 
 }
