02d8d5034fc85679d33e8b72f92351327b804892
Your Name <you@example.com>
2025-06-04 16:01:23 +0800

    fix:提交修改

--- app/admin/model/affiliationbm/AffiliationBmModel.php
+++ app/admin/model/affiliationbm/AffiliationBmModel.php
@@ -13,4 +13,17 @@ class AffiliationBmModel extends Model
 {
     protected $name = 'affiliation_bm';
 
+       // 自动写入时间戳字段
+    protected $autoWriteTimestamp = true;
+
+    public function getPutLableAttr($value, $data): array
+    {
+        if(empty($value)) return [];
+        return array_map('intval', explode(',', $value));
+    }
+
+    public function setPutLableAttr($value): string
+    {
+        return implode(',', $value);
+    }
 }
