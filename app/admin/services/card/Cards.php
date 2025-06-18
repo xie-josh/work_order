506b8b8b7dbd930f22cf7da5a737a8e938d0212f
Your Name <you@example.com>
2025-06-18 18:57:14 +0800

    fix:优化

--- app/admin/services/card/Cards.php
+++ app/admin/services/card/Cards.php
@@ -2,6 +2,8 @@
 namespace app\admin\services\card;
 
 use GuzzleHttp\Client;
+use think\facade\Db;
+use app\services\CardService;
 
 class Cards
 {
@@ -23,6 +25,26 @@ class Cards
         // $result = json_decode($result, true);
         // return $result;
         return [];
+    }
+
+    public function allCardFreeze($accountId)
+    {
+        $cardList = DB::table('ba_account_card')
+        ->alias('account_card')
+        ->field('cards_info.card_id,cards_info.account_id cards_account_id,cards_info.card_status')
+        ->leftJoin('ba_cards_info cards_info','cards_info.cards_id=account_card.cards_id')
+        ->where('account_card.account_id',$accountId)
+        ->select()->toArray();
+
+        foreach ($cardList as $v)
+        {
+            if($v['card_status'] == 'normal'){
+                $result = (new CardService($v['cards_account_id']))->cardFreeze(['card_id'=>$v['card_id']]);
+                if(isset($result['data']['cardStatus'])) DB::table('ba_cards_info')->where('card_id',$v['card_id'])->update(['card_status'=>$result['data']['cardStatus']]);
+            }
+
+        }
+        return ['code'=>1,'msg'=>'操作成功'];
     }
 
 }
