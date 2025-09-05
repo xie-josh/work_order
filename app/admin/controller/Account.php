55e0c15d9902f8c18d09978a56f0d8cc6c2ba1aa
Your Name <you@example.com>
2025-09-05 18:51:29 +0800

    fix:图片上传回退

--- app/admin/controller/Account.php
+++ app/admin/controller/Account.php
@@ -738,8 +738,8 @@ class Account extends Backend
                             }
                             if(!empty($bmDataList)) DB::table('ba_bm')->insertAll($bmDataList);
                     }
-                    if(!empty($ids)) $this->model->whereIn('id',array_column($ids,'id'))->update(['keep_succeed'=>1,'keep_time'=>time(),'update_time'=>time(),'operate_admin_id'=>$this->auth->id,'images'=>is_array($data['images']) ? implode(',', $data['images']) : $data['images']]);
-                }
+                    if(!empty($ids)) $this->model->whereIn('id',array_column($ids,'id'))->update(['keep_succeed'=>1,'keep_time'=>time(),'update_time'=>time(),'operate_admin_id'=>$this->auth->id]);
+                } //,'images'=>is_array($data['images']) ? implode(',', $data['images']) : $data['images']
                 //$this->model->whereIn('id',array_column($ids,'id'))->update(['money'=>0,'is_'=>1]);
                 $result = true;
                 $this->model->commit();
