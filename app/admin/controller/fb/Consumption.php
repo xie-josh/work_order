daf9dc7c88c905085732564be6749d10413b6459
Your Name <you@example.com>
2025-09-27 19:03:32 +0800

    fix:优化

--- app/admin/controller/fb/Consumption.php
+++ app/admin/controller/fb/Consumption.php
@@ -682,7 +682,7 @@ class Consumption extends Backend
                 $consumptionService = new \app\admin\services\fb\Consumption();
                 $totalDollar = $consumptionService->getTotalDollar($v['id']);
                 $thePreviousDayDollar = $consumptionService->thePreviousDay($v['id']); //前一天消耗总金额
-                $money = $v['money'] ?? 0;
+                $money = DB::table('ba_admin_money_log')->where('admin_id',$v['id'])->sum('money'); //总消耗
                 $remainingAmount = bcsub((string)$money,(string)$totalDollar,'2');
                 $dataList[] = [
                     'id' => $v['id'],
