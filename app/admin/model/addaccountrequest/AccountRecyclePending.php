abbb337ec13422c51f6132574786d8142a8ddf3e
Your Name <you@example.com>
2025-11-03 17:51:34 +0800

    fix:优化

---
+++ app/admin/model/addaccountrequest/AccountRecyclePending.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace app\admin\model\addaccountrequest;
+
+use think\Model;
+
+/**
+ * Accountrequest
+ */
+class AccountRecyclePending extends Model
+{
+    // 表名
+    protected $name = 'account_recycle_pending';
+
+    // 自动写入时间戳字段
+    protected $autoWriteTimestamp = true;
+
+
+}
