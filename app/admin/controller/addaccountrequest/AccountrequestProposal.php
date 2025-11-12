2cf8d9784c0eec385a4517a1db218308504d30d6
Your Name <you@example.com>
2025-11-12 15:20:28 +0800

    fix:优化

--- app/admin/controller/addaccountrequest/AccountrequestProposal.php
+++ app/admin/controller/addaccountrequest/AccountrequestProposal.php
@@ -26,7 +26,7 @@ class AccountrequestProposal extends Backend
 
     protected string|array $quickSearchField = ['id'];
 
-    protected array $noNeedPermission = ['index','accountRefresh','bmList','editStatusAll','Export','getAccountrequestProposal','getExportProgress',"accountCardBind","accountCardBindDel","accountCardList","accountCardHandoff","allEditLabelRelevance","manageExport","getManageExportProgress","getCountBalance"];
+    protected array $noNeedPermission = ['index','accountRefresh','bmList','editStatusAll','Export','getAccountrequestProposal','getExportProgress',"accountCardBind","accountCardBindDel","accountCardList","accountCardHandoff","allEditLabelRelevance","manageExport","getManageExportProgress","getCountBalance","allEditAccountName"];
 
     protected bool|string|int $dataLimit = 'parent';
 
