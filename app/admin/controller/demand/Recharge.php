5252e8e2b1c92d699369bb95f208b16273652a03
Your Name <you@example.com>
2025-11-19 18:49:43 +0800

    fix:优化

--- app/admin/controller/demand/Recharge.php
+++ app/admin/controller/demand/Recharge.php
@@ -73,12 +73,16 @@ class Recharge extends Backend
 
             $result = $res->toArray();
             $dataList = [];
-            $adminNameArr = DB::table('ba_admin')->column('username','id');
+            // $adminNameArr = DB::table('ba_admin')->column('username','id');
+            $adminNameArr = DB::table('ba_admin')->field('username,id,prepayment_type')->select()->toArray();
             if(!empty($result['data'])) {
                 $dataList = $result['data'];
-                             
+                $adminNameArr = array_column($adminNameArr,null,'id');
+                
+                // dd($adminNameArr);
                 foreach($dataList as &$v){
-                    $v['username'] = $adminNameArr[$v['admin_id']]??"";
+                    $v['username'] = $adminNameArr[$v['admin_id']]['username']??"";
+                    $v['prepayment_type'] = $adminNameArr[$v['admin_id']]['prepayment_type']??"";
                     if(in_array($v['account_id'],$wk)){
                         $v['wk_type'] = 1;
                         $v['wk_comment'] = "注意，注意，注意：该账户需要您自己去卡平台调整限额！！！！！！！";
