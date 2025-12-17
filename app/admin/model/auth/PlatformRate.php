6160ea313df5344e1307bf03e1ac0325d3d989f4
Your Name <you@example.com>
2025-12-17 19:49:12 +0800

    fix:提交

---
+++ app/admin/model/auth/PlatformRate.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace app\admin\model\auth;
+
+use think\Model;
+
+/**
+ * PlatformRate
+ */
+class PlatformRate extends Model
+{
+    // 表名
+    protected $name = 'platform_rate';
+
+    // 自动写入时间戳字段
+    protected $autoWriteTimestamp = true;
+
+}
