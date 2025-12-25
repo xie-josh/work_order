711dc0131c6006c91d203974cc88f6ba1e0a5136
jj <you@example.com>
2025-12-25 17:20:45 +0800

    fix:提交

--- app/command/MonthConsumptionTotalTask.php
+++ app/command/MonthConsumptionTotalTask.php
@@ -27,53 +27,58 @@ class MonthConsumptionTotalTask extends Command
         $output->writeln("统计月份". $month . PHP_EOL);
         //用户组
         $companyAll = DB::table('ba_company')->where(['status'=>1])->column('id');
-        foreach($companyAll as $company_id)
-        {
-            $consumptionResult = DB::table('ba_account_consumption')
-            ->field('dollar,date_start')
-            ->where('company_id',$company_id)
-            ->where('date_start', '>=', $start)
-            ->where('date_start', '<', $end)
-            ->select()->toArray();
-    
-            // $result = DB::table('ba_rate')->where('company_id',$adminId)->order('create_time asc')->select()->toArray();
-            $result = DB::table('ba_rate')->where('company_id',$company_id)->order('create_time asc')->select()->toArray();
-            $section = [];
-            if(!empty($result))foreach($result as $k => $v)
+        try{
+            foreach($companyAll as $company_id)
             {
-                $section[] =  ['start_tmie' => $v['create_time'],'end_tmie' => isset($result[$k+1]['create_time'])?$result[$k+1]['create_time']:'','rate'=>$v['rate']];
-            }
-            $section = array_reverse($section);
-            $total_dollar = 0;
-            $total_dollar_rate = 0;
-            foreach($consumptionResult AS $k => $v)
-            {
-                $total_dollar += $v['dollar'];
-                //服务费率
-                if(!empty($section))foreach($section as $kk => $vv)
+                $consumptionResult = DB::table('ba_account_consumption')
+                ->field('dollar,date_start')
+                ->where('company_id',$company_id)
+                ->where('date_start', '>=', $start)
+                ->where('date_start', '<', $end)
+                ->select()->toArray();
+        
+                // $result = DB::table('ba_rate')->where('company_id',$adminId)->order('create_time asc')->select()->toArray();
+                $result = DB::table('ba_rate')->where('company_id',$company_id)->order('create_time asc')->select()->toArray();
+                $section = [];
+                if(!empty($result))foreach($result as $k => $v)
                 {
-                    $start_tmie = strtotime($vv['start_tmie']);
-                    $end_tmie   = strtotime($vv['end_tmie']);
-                    //大于设定日期,设定当日不生效
-                    if (strtotime($v['date_start']) > $start_tmie && strtotime($v['date_start']) <= $end_tmie) {
-                    //  $dd[] =   $v['start_tmie'] .">$thsiTime 在区间内".$v['rate']."--".$v['end_tmie']."\n";
-                    $total_dollar_rate +=  $v['dollar']*$vv['rate'];
-                    // $output->writeln($vv['start_tmie'].">--"."命中区间".$v['date_start']."费率为".$vv['rate']."<=--".$vv['end_tmie']);
-                    }
-                    if (strtotime($v['date_start']) > $start_tmie && empty($end_tmie)) {
-                    //  $dd[] =  $v['start_tmie'] .">$thsiTime 没有结束时间".$v['rate']."--".$v['end_tmie']."\n";
-                    $total_dollar_rate +=  $v['dollar']*$vv['rate'];
-                    // $output->writeln($vv['start_tmie']."-->"."命中区间".$v['date_start']."费率为".$vv['rate']."No Null--".$vv['end_tmie']);
+                    $section[] =  ['start_tmie' => $v['create_time'],'end_tmie' => isset($result[$k+1]['create_time'])?$result[$k+1]['create_time']:'','rate'=>$v['rate']];
+                }
+                $section = array_reverse($section);
+                $total_dollar = 0;
+                $total_dollar_rate = 0;
+                foreach($consumptionResult AS $k => $v)
+                {
+                    $total_dollar += $v['dollar'];
+                    //服务费率
+                    if(!empty($section))foreach($section as $kk => $vv)
+                    {
+                        $start_tmie = strtotime($vv['start_tmie']);
+                        $end_tmie   = strtotime($vv['end_tmie']);
+                        //大于设定日期,设定当日不生效
+                        if (strtotime($v['date_start']) > $start_tmie && strtotime($v['date_start']) <= $end_tmie) {
+                        //  $dd[] =   $v['start_tmie'] .">$thsiTime 在区间内".$v['rate']."--".$v['end_tmie']."\n";
+                        $total_dollar_rate +=  $v['dollar']*$vv['rate'];
+                        // $output->writeln($vv['start_tmie'].">--"."命中区间".$v['date_start']."费率为".$vv['rate']."<=--".$vv['end_tmie']);
+                        }
+                        if (strtotime($v['date_start']) > $start_tmie && empty($end_tmie)) {
+                        //  $dd[] =  $v['start_tmie'] .">$thsiTime 没有结束时间".$v['rate']."--".$v['end_tmie']."\n";
+                        $total_dollar_rate +=  $v['dollar']*$vv['rate'];
+                        // $output->writeln($vv['start_tmie']."-->"."命中区间".$v['date_start']."费率为".$vv['rate']."No Null--".$vv['end_tmie']);
+                        }
                     }
                 }
-            }
+    
+                $archivedId = DB::table('ba_archived')->where([['month','=',$month],['company_id','=',$company_id]])->value('id');
+                if($archivedId) DB::table('ba_archived')->where('id',$archivedId)->delete();
+    
+                DB::table('ba_archived')->insert(['create_time'=>date("Y-m-d", time()),'month'=>$month,'company_id'=>$company_id,'month_total_dollar'=>round($total_dollar, 2),'rate_total_dollar'=>round($total_dollar_rate, 2)]); 
+                // 在这里编写你的定时任务逻辑
+                $output->writeln($company_id."用户".$month."总金额统计为$total_dollar"."服务费统计为$total_dollar_rate"."<br/>");
+          }
+        }catch(\Exception $e){
+            $this->error($e->getMessage());
+        }
 
-            $archivedId = DB::table('ba_archived')->where([['month','=',$month],['company_id','=',$company_id]])->value('id');
-            if($archivedId) DB::table('ba_archived')->where('id',$archivedId)->delete();
-
-            DB::table('ba_archived')->insert(['create_time'=>date("Y-m-d", time()),'month'=>$month,'company_id'=>$company_id,'month_total_dollar'=>round($total_dollar, 2),'rate_total_dollar'=>round($total_dollar_rate, 2)]); 
-            // 在这里编写你的定时任务逻辑
-            $output->writeln($company_id."用户".$month."总金额统计为$total_dollar"."服务费统计为$total_dollar_rate"."<br/>");
-      }
     }
 }
