e327d6bb8f39cb1bb69605589446864308047fb8
jj <you@example.com>
2026-03-31 20:01:45 +0800

    fix:优化

--- app/admin/controller/user/Industry.php
+++ app/admin/controller/user/Industry.php
@@ -25,5 +25,32 @@ class Industry extends Backend
         parent::initialize();
         $this->model = new IndustryModel();
     }
+
+       /**
+     * 查看
+     * @throws Throwable
+     */
+    public function index(): void
+    {
+        if ($this->request->param('select')) {
+            $this->select();
+        }
+
+        list($where, $alias, $limit, $order) = $this->queryBuilder();
+        $res = $this->model
+            ->field($this->indexField)
+            ->withJoin($this->withJoinTable, $this->withJoinType)
+            ->alias($alias)
+            ->where($where)
+            ->order('update_time','ase')
+            ->paginate($limit);
+
+        $this->success('', [
+            'list'   => $res->items(),
+            'total'  => $res->total(),
+            'remark' => get_route_remark(),
+        ]);
+    }
+
     
 }
